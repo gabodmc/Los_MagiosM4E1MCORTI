2302372ea579e99d5cff0110e6763fc639b706c0
gabodmc <gabrielmc@live.com.ar>
2021-04-02 16:09:55 -0300

    Agregando secciones de html y actualizando el app.js

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/registrer.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
 app.listen(port, () => {
     console.log('App funcionando en puerto '+ port);
 });
